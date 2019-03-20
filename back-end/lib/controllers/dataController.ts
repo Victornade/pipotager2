deab6ded55097c7af498d1972dc81d2c8bad58f6
Victor Le Deuff <victor.ledeuff@energies-demain.com>
2019-03-20 15:34:20 +0100

    CHANGE PORT BACKEND TO 3001

--- back-end/lib/controllers/dataController.ts
+++ back-end/lib/controllers/dataController.ts
@@ -21,15 +21,15 @@ export class DataController {
     }
     getDatas(req, res) {
         console.log('get all');
-        Data.find({}, (err, data) => {
+        Data.find({date:{$gte:new Date("2019-02-17")}}, (err, data) => {
             if (err) {
                 res.send(err);
                 return;
             }
 
             var toSend = [
-                { name: "temperature", fillColor: 'rgb(	255, 0, 0,0.5)', color: 'rgb(255, 0, 0,1)',   data: [] , yAxis:1, type: 'line', turboThreshold:0 },
-                { name: "humidite_air", fillColor:'	rgb(65, 105, 225,0.5)', color: 'rgb(65, 105, 225,1)',   data: [] , yAxis:0, type: 'line', turboThreshold:0 },
+                { name: "temperature", fillColor: 'rgb(	255, 0, 0,0.5)', color: 'rgb(255, 0, 0,0.5)',   data: [] , yAxis:1, type: 'area', turboThreshold:0 },
+                { name: "humidite_air", fillColor:'	rgb(65, 105, 225,0.5)', color: 'rgb(65, 105, 225,0.5)',   data: [] , yAxis:0, type: 'area', turboThreshold:0 },
                 { name: "humidite_1",   data: [] , type: 'line', turboThreshold:0 },
                 { name: "humidite_2",   data: [] , type: 'line', turboThreshold:0 },
                 { name: "humidite_3",   data: [] , type: 'line', turboThreshold:0 },
